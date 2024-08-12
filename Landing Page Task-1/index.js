ff22229b4f9d15229c6ed0385be0ed0cd186313c
Prashant Baghel <50138458+prashantbaghel98@users.noreply.github.com>
2024-08-12 10:29:34 +0530

    Add files via upload

---
+++ Landing Page Task-1/index.js
@@ -0,0 +1,37 @@
+// let toggles= document.getElementsByClassName("toggle-icon")
+// let menulist= document.querySelectorAll(".menu-list a")
+// let btn = document.querySelector(".mobile-menu")
+// let menu= menulist.forEach(element => {
+//     btn.append(element.innerHTML)
+//     console.log(element.innerHTML)
+// });
+
+// btn.style.color="white";
+
+
+// let mobilebtn = btn.style.visibility = "hidden"
+
+
+
+// toggles= document.addEventListener("click",()=>{
+//     if(toggles===false){
+//         btn.style.visibility = "visible"
+//         toggles=true;
+//     }
+
+//     else{
+//         toggles=false;
+//         btn.style.visibility = "hidden"
+//     }
+
+// });
+
+
+
+
+
+
+
+
+
+
